3bc7bf20f9564471aa227462dc0a571545a80f6e
Fernando dos Santos <nando4803@gmail.com>
2023-08-24 00:09:04 +0100

    docs: add documentation for create Opening

--- handler/response.go
+++ handler/response.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/buzzcoode/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,4 +22,14 @@ func sendSucess(ctx *gin.Context, op string, data interface{}) {
 		"message": fmt.Sprintf("operation from handler: %s successfull", op),
 		"data":    data,
 	})
+}
+
+type ErrorResponse struct {
+	Message   string `json:"message"`
+	ErrorCode int    `json:"errorCode"`
+}
+
+type CreateOpeningResponse struct {
+	Message string                  `json: "message"`
+	Data    schemas.OpeningResponse `json: "data"`
 }
