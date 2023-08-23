5d9548e50f582a64e929d68890fea433a7e0fc03
Fernando dos Santos <nando4803@gmail.com>
2023-08-23 17:16:04 +0100

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/buzzcoode/gopportunities/router"
+import (
+	"github.com/buzzcoode/gopportunities/config"
+	"github.com/buzzcoode/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+
+	}
+
 	// Initialize Router
 	router.Initialize()
 }
