13f5acad1e0f77123106c8d1606ac9708d58ecfe
Fernando dos Santos <nando4803@gmail.com>
2023-08-24 00:34:41 +0100

    fix: header ctx content-type text

--- handler/response.go
+++ handler/response.go
@@ -17,7 +17,7 @@ func sendError(ctx *gin.Context, code int, msg string) {
 }
 
 func sendSucess(ctx *gin.Context, op string, data interface{}) {
-	ctx.Header("content-type", "application")
+	ctx.Header("Content-type", "application/json")
 	ctx.JSON(http.StatusOK, gin.H{
 		"message": fmt.Sprintf("operation from handler: %s successfull", op),
 		"data":    data,
